f5f2c8d23beb1fe539d3ccc8f0efecc41e724128
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2024-12-28 17:20:33 +0530

    dc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,38 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: { // Add the required parameter
+    eslint: true, // Enables ESLint recommended rules
+  },
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "next/core-web-vitals",
+    "next/typescript"
+  ),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: ["@typescript-eslint", "react"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,173 +2,182 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <>
-      <div className="w-full  flex flex-col items-center justify-center">
-        <header className="flex w-full justify-end mr-28 ">
-          <Link
-            href="/login"
-            className="m-2 bg-white text-black px-3 py-1 rounded-xl "
-          >
-            Login
-          </Link>
-          <Link
-            href="/signup"
-            className="m-2 bg-white text-black px-3 py-1 rounded-xl "
-          >
-            Signup
-          </Link>
-        </header>
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <div className="w-full flex flex-col md:flex-row items-center justify-evenly m-4">
-          <div className="bg-white h-auto w-60 flex flex-col items-center justify-center rounded-lg text-black m-4">
-            <p className="text-xl">Developed By</p>
-            <img
-              src="https://github.com/nikhilnandanwar429.png"
-              alt="github avater"
-              width={180}
-              height={180}
-              className="w-full h-auto p-2 rounded-2xl"
-            />
-            
-            <ul className="flex justify-evenly">
-              <li className="mx-2 mb-2">
-                <Link
-                  href="https://github.com/nikhilnandanwar429"
-                  target="_blank"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24px"
-                    height="24px"
-                  >
-                    {" "}
-                    <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6c0-0.4,0-0.9,0.2-1.3 C7.2,6.1,7.4,6,7.5,6c0,0,0.1,0,0.1,0C8.1,6.1,9.1,6.4,10,7.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3c0.9-0.9,2-1.2,2.5-1.3 c0,0,0.1,0,0.1,0c0.2,0,0.3,0.1,0.4,0.3C17,6.7,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4 c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3C22,6.1,16.9,1.4,10.9,2.1z" />
-                  </svg>
-                </Link>
-              </li>
-              <li className="mx-2 mb-2">
-                <Link
-                  href="https://www.linkedin.com/in/nikhilnandanwar429/"
-                  target="_blank"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24px"
-                    height="24px"
-                  >
-                    {" "}
-                    <path d="M19,3H5C3.895,3,3,3.895,3,5v14c0,1.105,0.895,2,2,2h14c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z M9,17H6.477v-7H9 V17z M7.694,8.717c-0.771,0-1.286-0.514-1.286-1.2s0.514-1.2,1.371-1.2c0.771,0,1.286,0.514,1.286,1.2S8.551,8.717,7.694,8.717z M18,17h-2.442v-3.826c0-1.058-0.651-1.302-0.895-1.302s-1.058,0.163-1.058,1.302c0,0.163,0,3.826,0,3.826h-2.523v-7h2.523v0.977 C13.93,10.407,14.581,10,15.802,10C17.023,10,18,10.977,18,13.174V17z" />
-                  </svg>
-                </Link>
-              </li>
-              <li className="mx-2 mb-2">
-                <Link href="https://x.com/NikhilNand429" target="_blank">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24px"
-                    height="24px"
-                  >
-                    <path d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z" />
-                  </svg>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="bg-white h-auto w-60 flex flex-col items-center justify-center rounded-lg text-black m-4">
-            <p className="text-xl">Taught By</p>
-            <img
-              src="https://github.com/hiteshchoudhary.png"
-              alt="github avater"
-              className="p-2 rounded-2xl"
-            />
-            <ul className="flex justify-evenly">
-              <li className="mx-2 mb-2">
-                <Link href="https://github.com/hiteshchoudhary" target="_blank">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24px"
-                    height="24px"
-                  >
-                    {" "}
-                    <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6c0-0.4,0-0.9,0.2-1.3 C7.2,6.1,7.4,6,7.5,6c0,0,0.1,0,0.1,0C8.1,6.1,9.1,6.4,10,7.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3c0.9-0.9,2-1.2,2.5-1.3 c0,0,0.1,0,0.1,0c0.2,0,0.3,0.1,0.4,0.3C17,6.7,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4 c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3C22,6.1,16.9,1.4,10.9,2.1z" />
-                  </svg>
-                </Link>
-              </li>
-              <li className="mx-2 mb-2">
-                <Link
-                  href="https://www.linkedin.com/in/hiteshchoudhary/"
-                  target="_blank"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24px"
-                    height="24px"
-                  >
-                    {" "}
-                    <path d="M19,3H5C3.895,3,3,3.895,3,5v14c0,1.105,0.895,2,2,2h14c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z M9,17H6.477v-7H9 V17z M7.694,8.717c-0.771,0-1.286-0.514-1.286-1.2s0.514-1.2,1.371-1.2c0.771,0,1.286,0.514,1.286,1.2S8.551,8.717,7.694,8.717z M18,17h-2.442v-3.826c0-1.058-0.651-1.302-0.895-1.302s-1.058,0.163-1.058,1.302c0,0.163,0,3.826,0,3.826h-2.523v-7h2.523v0.977 C13.93,10.407,14.581,10,15.802,10C17.023,10,18,10.977,18,13.174V17z" />
-                  </svg>
-                </Link>
-              </li>
-              <li className="mx-2 mb-2">
-                <Link href="https://x.com/Hiteshdotcom" target="_blank">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24px"
-                    height="24px"
-                  >
-                    <path d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z" />
-                  </svg>
-                </Link>
-              </li>
-              <li className="mx-2 mb-2">
-                <Link
-                  href="https://www.youtube.com/c/HiteshChoudharydotcom"
-                  target="_blank"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24px"
-                    height="24px"
-                  >
-                    {" "}
-                    <path d="M21.582,6.186c-0.23-0.86-0.908-1.538-1.768-1.768C18.254,4,12,4,12,4S5.746,4,4.186,4.418 c-0.86,0.23-1.538,0.908-1.768,1.768C2,7.746,2,12,2,12s0,4.254,0.418,5.814c0.23,0.86,0.908,1.538,1.768,1.768 C5.746,20,12,20,12,20s6.254,0,7.814-0.418c0.861-0.23,1.538-0.908,1.768-1.768C22,16.254,22,12,22,12S22,7.746,21.582,6.186z M10,15.464V8.536L16,12L10,15.464z" />
-                  </svg>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+	return (
+		<>
+			<div className="w-full  flex flex-col items-center justify-center">
+				<header className="flex w-full justify-end mr-28 ">
+					<Link
+						href="/login"
+						className="m-2 bg-white text-black px-3 py-1 rounded-xl "
+					>
+						Login
+					</Link>
+					<Link
+						href="/signup"
+						className="m-2 bg-white text-black px-3 py-1 rounded-xl "
+					>
+						Signup
+					</Link>
+				</header>
+				<Image
+					className="dark:invert"
+					src="/next.svg"
+					alt="Next.js logo"
+					width={180}
+					height={38}
+					priority
+				/>
+				<div className="w-full flex flex-col md:flex-row items-center justify-evenly m-4">
+					<div className="bg-white h-auto w-60 flex flex-col items-center justify-center rounded-lg text-black m-4">
+						<p className="text-xl">Developed By</p>
+						<img
+							src="https://github.com/nikhilnandanwar429.png"
+							alt="github avater"
+							width={180}
+							height={180}
+							className="w-full h-auto p-2 rounded-2xl"
+						/>
 
-        <div>
-          <p>
-            A big thank you to{" "}
-            <u>
-              <Link
-                href="https://www.youtube.com/c/HiteshChoudharydotcom"
-                target="_blank"
-              >
-                Hitesh Choudhary Sir
-              </Link>{" "}
-            </u>
-            for his amazing Next.js tutorials and guidance!
-          </p>
-        </div>
-      </div>
-    </>
-  );
+						<ul className="flex justify-evenly">
+							<li className="mx-2 mb-2">
+								<Link
+									href="https://github.com/nikhilnandanwar429"
+									target="_blank"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24px"
+										height="24px"
+									>
+										{" "}
+										<path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6c0-0.4,0-0.9,0.2-1.3 C7.2,6.1,7.4,6,7.5,6c0,0,0.1,0,0.1,0C8.1,6.1,9.1,6.4,10,7.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3c0.9-0.9,2-1.2,2.5-1.3 c0,0,0.1,0,0.1,0c0.2,0,0.3,0.1,0.4,0.3C17,6.7,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4 c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3C22,6.1,16.9,1.4,10.9,2.1z" />
+									</svg>
+								</Link>
+							</li>
+							<li className="mx-2 mb-2">
+								<Link
+									href="https://www.linkedin.com/in/nikhilnandanwar429/"
+									target="_blank"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24px"
+										height="24px"
+									>
+										{" "}
+										<path d="M19,3H5C3.895,3,3,3.895,3,5v14c0,1.105,0.895,2,2,2h14c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z M9,17H6.477v-7H9 V17z M7.694,8.717c-0.771,0-1.286-0.514-1.286-1.2s0.514-1.2,1.371-1.2c0.771,0,1.286,0.514,1.286,1.2S8.551,8.717,7.694,8.717z M18,17h-2.442v-3.826c0-1.058-0.651-1.302-0.895-1.302s-1.058,0.163-1.058,1.302c0,0.163,0,3.826,0,3.826h-2.523v-7h2.523v0.977 C13.93,10.407,14.581,10,15.802,10C17.023,10,18,10.977,18,13.174V17z" />
+									</svg>
+								</Link>
+							</li>
+							<li className="mx-2 mb-2">
+								<Link
+									href="https://x.com/NikhilNand429"
+									target="_blank"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24px"
+										height="24px"
+									>
+										<path d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z" />
+									</svg>
+								</Link>
+							</li>
+						</ul>
+					</div>
+					<div className="bg-white h-auto w-60 flex flex-col items-center justify-center rounded-lg text-black m-4">
+						<p className="text-xl">Taught By</p>
+						<img
+							src="https://github.com/hiteshchoudhary.png"
+							alt="github avater"
+							className="p-2 rounded-2xl"
+						/>
+						<ul className="flex justify-evenly">
+							<li className="mx-2 mb-2">
+								<Link
+									href="https://github.com/hiteshchoudhary"
+									target="_blank"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24px"
+										height="24px"
+									>
+										{" "}
+										<path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6c0-0.4,0-0.9,0.2-1.3 C7.2,6.1,7.4,6,7.5,6c0,0,0.1,0,0.1,0C8.1,6.1,9.1,6.4,10,7.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3c0.9-0.9,2-1.2,2.5-1.3 c0,0,0.1,0,0.1,0c0.2,0,0.3,0.1,0.4,0.3C17,6.7,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4 c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3C22,6.1,16.9,1.4,10.9,2.1z" />
+									</svg>
+								</Link>
+							</li>
+							<li className="mx-2 mb-2">
+								<Link
+									href="https://www.linkedin.com/in/hiteshchoudhary/"
+									target="_blank"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24px"
+										height="24px"
+									>
+										{" "}
+										<path d="M19,3H5C3.895,3,3,3.895,3,5v14c0,1.105,0.895,2,2,2h14c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z M9,17H6.477v-7H9 V17z M7.694,8.717c-0.771,0-1.286-0.514-1.286-1.2s0.514-1.2,1.371-1.2c0.771,0,1.286,0.514,1.286,1.2S8.551,8.717,7.694,8.717z M18,17h-2.442v-3.826c0-1.058-0.651-1.302-0.895-1.302s-1.058,0.163-1.058,1.302c0,0.163,0,3.826,0,3.826h-2.523v-7h2.523v0.977 C13.93,10.407,14.581,10,15.802,10C17.023,10,18,10.977,18,13.174V17z" />
+									</svg>
+								</Link>
+							</li>
+							<li className="mx-2 mb-2">
+								<Link
+									href="https://x.com/Hiteshdotcom"
+									target="_blank"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24px"
+										height="24px"
+									>
+										<path d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z" />
+									</svg>
+								</Link>
+							</li>
+							<li className="mx-2 mb-2">
+								<Link
+									href="https://www.youtube.com/c/HiteshChoudharydotcom"
+									target="_blank"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24px"
+										height="24px"
+									>
+										{" "}
+										<path d="M21.582,6.186c-0.23-0.86-0.908-1.538-1.768-1.768C18.254,4,12,4,12,4S5.746,4,4.186,4.418 c-0.86,0.23-1.538,0.908-1.768,1.768C2,7.746,2,12,2,12s0,4.254,0.418,5.814c0.23,0.86,0.908,1.538,1.768,1.768 C5.746,20,12,20,12,20s6.254,0,7.814-0.418c0.861-0.23,1.538-0.908,1.768-1.768C22,16.254,22,12,22,12S22,7.746,21.582,6.186z M10,15.464V8.536L16,12L10,15.464z" />
+									</svg>
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div>
+					<p>
+						A big thank you to{" "}
+						<u>
+							<Link
+								href="https://www.youtube.com/c/HiteshChoudharydotcom"
+								target="_blank"
+							>
+								Hitesh Choudhary Sir
+							</Link>{" "}
+						</u>
+						for his amazing Next.js tutorials and guidance!
+					</p>
+				</div>
+			</div>
+		</>
+	);
 }
